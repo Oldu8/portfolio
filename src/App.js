b73bb2b8ef52116de463cd28509663a6c3623e67
oldu8 <dudkoleg@gmail.com>
2022-10-17 21:14:21 -0400

    footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import AboutBlock from './components/AboutBlock/AboutBlock';
 import CVBlock from './components/CVBlock/CVBlock';
+import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import HeroBlock from './components/HeroBlock/HeroBlock';
 import ProjectsBlock from './components/ProjectsBlock/ProjectsBlock';
@@ -13,6 +14,7 @@ function App() {
       <AboutBlock />
       <ProjectsBlock />
       <CVBlock />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './Footer.module.scss'
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <h3 className={styles.title}>Thanks for your time!</h3>
+            <a className={styles.btn} href="/hero">Up!</a>
+        </footer>
+    )
+}
+
+export default Footer
