4460ee3f3129a738807973e490dd7625a4635756
oldu8 <dudkoleg@gmail.com>
2022-11-28 15:36:08 -0500

    stars bg

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ import ProjectsBlock from './components/ProjectsBlock/ProjectsBlock';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" >
+      <div id='stars2'></div>
       <Header />
       <HeroBlock />
       <AboutBlock />
