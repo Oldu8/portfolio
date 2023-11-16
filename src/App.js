647aa8031d9dd6c9cffbe91c6da0bca68236a927
oleh-runzy <dudkoleg@gmail.com>
2023-11-16 21:55:33 +0200

    upd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.module.scss';
 import CVBlock from './components/CVBlock/CVBlock';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import HeroBlock from './components/HeroBlock/HeroBlock';
 import ProjectsBlock from './components/ProjectsBlock/ProjectsBlock';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
@@ -14,8 +14,8 @@ function App() {
         <Header />
         <Routes>
           <Route exact path='/' element={<HeroBlock />}></Route>
-          <Route exact path='/projects/' element={<ProjectsBlock />}></Route>
-          <Route exact path='/cv/' element={<CVBlock />}></Route>
+          <Route path='/projects/' element={<ProjectsBlock />}></Route>
+          <Route path='/cv/' element={<CVBlock />}></Route>
         </Routes>
         <Footer />
       </div>
