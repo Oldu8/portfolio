efdc0d2a1da3a1818d307e151396d01e1b8394d5
Oleh <dudkoleg@gmail.com>
2024-03-10 11:45:10 +0200

    info upd

--- src/components/ProjectsBlock/ProjectsBlock.js
+++ src/components/ProjectsBlock/ProjectsBlock.js
@@ -1,28 +1,27 @@
-import React from 'react'
-import { projectsArr } from '../../assets/projectsArr.js'
-import styles from './ProjectsBlock.module.scss'
+import React from "react";
+import { projectsArr } from "../../assets/projectsArr.js";
+import styles from "./ProjectsBlock.module.scss";
 
-import Card from './Card/Card'
+import Card from "./Card/Card";
 
 const ProjectsBlock = () => {
+  return (
+    <section className={styles.projectsWrapper}>
+      <div className={styles.content}>
+        <h2 className={styles.title}>My projects portfolio</h2>
+        <p className={styles.description}>
+          Before you check I need to say, that I more than a year didn't create
+          any valuable project that I want to share, so here some projects that
+          I created aside from main job. So you can find something interesting.
+        </p>
+        <div className={styles.projectsCards}>
+          {projectsArr.map((i) => {
+            return <Card item={i} key={i.title} />;
+          })}
+        </div>
+      </div>
+    </section>
+  );
+};
 
-    return (
-        <section className={styles.projectsWrapper}>
-            <div className={styles.content}>
-                <h2 className={styles.title}>
-                    My projects portfolio
-                </h2>
-                <p className={styles.description}>
-                    Here some projects that I created aside from main job. So you can find something interesting.
-                </p>
-                <div className={styles.projectsCards}>
-                    {projectsArr.map((i) => {
-                        return <Card item={i} key={i.title} />
-                    })}
-                </div>
-            </div>
-        </section>
-    )
-}
-
-export default ProjectsBlock
+export default ProjectsBlock;
