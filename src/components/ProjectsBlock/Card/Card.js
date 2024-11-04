c4552132d9a9a4283262543a770947263596b848
Oleh <dudkoleg@gmail.com>
2024-11-04 18:58:22 +0200

    upd ui

--- src/components/ProjectsBlock/Card/Card.js
+++ src/components/ProjectsBlock/Card/Card.js
@@ -1,35 +1,34 @@
-import React from 'react'
-import styles from './Card.module.scss'
-
+import React from "react";
+import styles from "./Card.module.scss";
 
 function Card({ item }) {
-    const { title, img, description, techs, link } = item;
+  const { title, img, description, techs, link, year } = item;
 
-    return (
-        <div className={styles.card}>
-            <div className={styles.imgContainer}>
-                <img className={styles.img} src={img} alt={`${title} screen`} />
-            </div>
-            <div className={styles.info}>
-                <div className={styles.infoText}>
-                    <h3 className={styles.subtitle}>
-                        {title}
-                    </h3>
-                    <p className={styles.techList}>
-                        {techs}
-                    </p>
-                    <p className={styles.description}>
-                        {description}
-                    </p>
-                </div>
-                <a href={link} target="_blank" rel="noreferrer" className={styles.btnWrapper}>
-                    <button className={styles.button}>
-                        Check website
-                    </button>
-                </a>
-            </div>
+  return (
+    <div className={styles.card}>
+      <div className={styles.imgContainer}>
+        <img className={styles.img} src={img} alt={`${title} screen`} />
+      </div>
+      <div className={styles.info}>
+        <div className={styles.infoText}>
+          <h3 className={styles.subtitle}>{title}</h3>
+          <p className={styles.techList}>
+            Techs: <strong>{techs}</strong>
+          </p>
+          <p className={styles.description}>Description: {description}</p>
+          <p className={styles.description}>Year: {year}</p>
         </div>
-    )
+        <a
+          href={link}
+          target="_blank"
+          rel="noreferrer"
+          className={styles.btnWrapper}
+        >
+          <button className={styles.button}>Check website</button>
+        </a>
+      </div>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
